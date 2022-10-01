00cd6017bc52598c0849446b567c08b9a0b2e7a3
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2022-10-01 14:59:18 -0400

    Define UI Manager

--- CubeRun.Unity/Assets/Scripts/UIManager.cs
+++ CubeRun.Unity/Assets/Scripts/UIManager.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public string Score;
+
+    private GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.gameManager = GameObject.FindObjectOfType<GameManager>();
+
+        StartCoroutine("DisplayUI");
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator DisplayUI()
     {
-        
+        DisplayGameUI();
+        yield return new WaitUntil(() => gameManager.IsGameOver);
+        DisplayGameOverUI();
+    }
+
+    private void DisplayGameUI()
+    {
+        Debug.Log($"Displaying game UI with score {Score}");
+    }
+
+    private void DisplayGameOverUI()
+    {
+        Debug.Log($"Displaying game over UI with score {Score}");
     }
 }
